1545da77ac1ccf76e51da04c62c98dc3841ffe52
MickeyClub <972207518@qq.com>
2018-09-03 20:24:29 +0800

    first commot

---
+++ public/m/js/index.js
@@ -0,0 +1,37 @@
+$(function () {
+    var letao = new Letao();
+    // letao.initSlide();
+    letao.initScroll()
+})
+
+var Letao = function () {
+
+}
+
+Letao.prototype = {
+    // 初始化轮播图方法
+    inintSlide: function () {
+        //获得slider插件对象
+        var gallery = mui('.mui-slider');
+        gallery.slider({
+            interval: 5000 //自动轮播周期，若为0则不自动播放，默认为0；
+        });
+    },
+    // 初始化区域滚动
+    initScroll: function () {
+        mui('.mui-scroll-wrapper').scroll({
+            deceleration: 0.0005, //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006,
+            
+            scrollY: true, //是否竖向滚动
+            scrollX: false, //是否横向滚动
+            startX: 0, //初始化时滚动至x
+            startY: 0, //初始化时滚动至y
+            indicators: true, //是否显示滚动条
+            deceleration:0.0006, //阻尼系数,系数越小滑动越灵敏
+            bounce: true //是否启用回弹
+
+            // 以上都是默认值 如果都一样可以不设置参数
+
+        });
+    }
+}
